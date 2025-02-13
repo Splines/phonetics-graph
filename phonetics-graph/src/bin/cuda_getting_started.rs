f4d72a5d48857e175e8522343b673ec1001a5a26
Splines <dominic-plein@gmx.de>
2025-02-13 16:05:22 +0100

    Outsource kernel to separate file

--- phonetics-graph/src/bin/cuda_getting_started.rs
+++ phonetics-graph/src/bin/cuda_getting_started.rs
@@ -1,21 +1,20 @@
+use std::fs;
+
 use cudarc::driver::{LaunchAsync, LaunchConfig};
+
+static KERNEL_FILE: &str = "./src/kernels/sin_kernel.cpp";
 
 fn getting_started() -> Result<(), Box<dyn std::error::Error>> {
     let dev = cudarc::driver::CudaDevice::new(0)?;
     println!("Device name: {}", dev.name()?);
 
     println!("Allocating buffers");
-    let inp = dev.htod_copy(vec![1.0f32; 10000000])?;
-    let mut out = dev.alloc_zeros::<f32>(10000000)?;
+    let inp = dev.htod_copy(vec![1.0f32; 100])?;
+    let mut out = dev.alloc_zeros::<f32>(100)?;
 
-    let ptx = cudarc::nvrtc::compile_ptx(
-"extern \"C\" __global__ void sin_kernel(float *out, const float *inp, const size_t numel) {
-    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < numel) {
-        out[i] = sin(inp[i]);
-    }
-}"
-)?;
+    let kernel_code = fs::read_to_string(KERNEL_FILE).unwrap();
+    assert!(kernel_code.contains("sin_kernel"), "Kernel code does not contain 'sin_kernel'");
+    let ptx = cudarc::nvrtc::compile_ptx(kernel_code)?;
 
     println!("Loading PTX");
     dev.load_ptx(ptx, "my_module", &["sin_kernel"])?;
@@ -23,13 +22,13 @@ fn getting_started() -> Result<(), Box<dyn std::error::Error>> {
     println!("Retrieving kernel");
 
     let sin_kernel = dev.get_func("my_module", "sin_kernel").unwrap();
-    let cfg = LaunchConfig::for_num_elems(10000000);
+    let cfg = LaunchConfig::for_num_elems(100);
 
     println!("Launching kernel");
-    unsafe { sin_kernel.launch(cfg, (&mut out, &inp, 10000000usize)) }?;
+    unsafe { sin_kernel.launch(cfg, (&mut out, &inp, 100usize)) }?;
 
     let out_host: Vec<f32> = dev.dtoh_sync_copy(&out)?;
-    assert_eq!(out_host.len(), 10000000);
+    assert_eq!(out_host.len(), 100);
 
     println!("Done");
     Ok(())
