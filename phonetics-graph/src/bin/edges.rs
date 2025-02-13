87c19d2b77c303e7f398493285aaa8945cf1144b
Splines <dominic-plein@gmx.de>
2025-02-13 19:18:23 +0100

    Init first version of word passing

--- phonetics-graph/src/bin/edges.rs
+++ phonetics-graph/src/bin/edges.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::fs::{self, File};
+use std::io::{self, BufRead};
 
 use cudarc::driver::{LaunchAsync, LaunchConfig};
 
@@ -6,19 +7,39 @@ static KERNEL_FILE: &str = "./src/kernels/needleman_wunsch.cpp";
 static MODULE_NAME: &str = "phonetics_module";
 static KERNEL_NAME: &str = "needleman_wunsch";
 
-fn compute() -> Result<(), Box<dyn std::error::Error>> {
+static THRESHOLD: u8 = 100;
+
+fn read_words_from_csv(file_path: &str) -> io::Result<Vec<Vec<u8>>> {
+    let mut words = Vec::new();
+    let file = File::open(file_path)?;
+    for line in io::BufReader::new(file).lines() {
+        let line = line?;
+        let word: Vec<u8> = line
+            .split(',')
+            .map(|s| s.trim().parse().expect("Failed to parse integer"))
+            .collect();
+        words.push(word);
+    }
+    Ok(words)
+}
+
+fn compute(words: Vec<Vec<u8>>) -> Result<(), Box<dyn std::error::Error>> {
     let dev = cudarc::driver::CudaDevice::new(0)?;
     println!("Device name: {}", dev.name()?);
 
     let num_nodes: usize = 5;
     let num_adjacency_matrix_elements: usize = (num_nodes * (num_nodes + 1)) / 2;
-    println!(
-        "num_adjacency_matrix_elements: {}",
-        num_adjacency_matrix_elements
-    );
+    println!("num_adjacency_matrix_elements: {num_adjacency_matrix_elements}");
+
+    let words_flat: Vec<u8> = words.iter().flat_map(|w| w.iter()).copied().collect();
+    let words_lengths: Vec<usize> = words.iter().map(|w| w.len()).collect();
+
+    // copy that data to the global memory of the GPU
+    let words_flat_device = dev.htod_copy(words_flat)?;
+    let words_lengths_device = dev.htod_copy(words_lengths)?;
+
 
     println!("Allocating buffers");
-    let inp = dev.htod_copy(vec![1.0f32; num_adjacency_matrix_elements])?;
     let mut out = dev.alloc_zeros::<f32>(num_adjacency_matrix_elements)?;
 
     let mut kernel_code = fs::read_to_string(KERNEL_FILE).unwrap();
@@ -31,7 +52,8 @@ fn compute() -> Result<(), Box<dyn std::error::Error>> {
     let cfg = LaunchConfig::for_num_elems(num_adjacency_matrix_elements as u32);
 
     println!("Launching kernel");
-    unsafe { kernel.launch(cfg, (&mut out, &inp, 42usize)) }?;
+    unsafe { kernel.launch(cfg, (&mut out, &words_flat_device, &words_lengths_device,
+        words.len() as u32)) }?;
 
     let out_host: Vec<f32> = dev.dtoh_sync_copy(&out)?;
     assert_eq!(out_host.len(), num_adjacency_matrix_elements);
@@ -42,7 +64,15 @@ fn compute() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn main() {
-    if let Err(e) = compute() {
+    let words = read_words_from_csv("../data/graph/french-phonetics-integers.txt")
+        .expect("Failed to read words from CSV");
+    println!("Num words: {}", words.len());
+
+    // only consider the first THRESHOLD words
+    let words_shortened: Vec<Vec<u8>> = words.into_iter().take(THRESHOLD as usize).collect();
+    assert_eq!(words_shortened.len(), THRESHOLD as usize);
+
+    if let Err(e) = compute(words_shortened) {
         eprintln!("Error: {}", e);
     }
 }
--- phonetics-graph/src/kernels/needleman_wunsch.cpp
+++ phonetics-graph/src/kernels/needleman_wunsch.cpp
@@ -1,4 +1,8 @@
-extern "C" __global__ void needleman_wunsch(float *out)
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+
+extern "C" __global__ void needleman_wunsch(
+    float *out, uint8_t *words_flat, uint8_t *words_lengths, const uint32_t num_nodes)
 {
     unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -8,5 +12,7 @@ extern "C" __global__ void needleman_wunsch(float *out)
     unsigned int s = row * (z - row / 2);
     unsigned int col = row + idx - s;
 
-    out[idx] = row * 100 + col; // dummy
+    // out[idx] = row * 100 + col; // dummy
+    // out[0] = words_flat[0];
+    out[0] = num_nodes;
 }
