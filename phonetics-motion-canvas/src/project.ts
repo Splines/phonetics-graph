b0c761450e364fc805cf94eba2abc4f6623922a1
Splines <info@splines.me>
2025-04-09 19:28:25 +0200

    Init motion canvas project

---
+++ phonetics-motion-canvas/src/project.ts
@@ -0,0 +1,12 @@
+import {makeProject} from '@motion-canvas/core';
+
+import example from './scenes/example?scene';
+
+import './global.css';
+
+export default makeProject({
+  scenes: [example],
+  variables: {
+    textFill: "white"
+  }
+});
