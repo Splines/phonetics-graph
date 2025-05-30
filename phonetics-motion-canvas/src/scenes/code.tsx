5ef985efa3dd57e2bff1ccca591946445f5339bf
Splines <info@splines.me>
2025-05-30 11:51:05 +0200

    Add latex helper variables

--- phonetics-motion-canvas/src/scenes/code.tsx
+++ phonetics-motion-canvas/src/scenes/code.tsx
@@ -1,8 +1,10 @@
 import { Latex, makeScene2D, Rect } from "@motion-canvas/2d";
-import { TEXT_FILL } from "./globals";
+import { TEXT_FILL, TEXT_FILL_FADED } from "./globals";
 
 // projection resolution: 1920x2160 (half screen in 4K)
 export default makeScene2D(function* (view) {
+  // 🎈 Words as integer list
+
   const words = `\\begin{align}
   a &= \\bigl[0, 1, 2, 3, 4, 3\\bigr] \\\\
   b &= \\bigl[5, 1, 4, 3\\bigr]
@@ -20,7 +22,7 @@ export default makeScene2D(function* (view) {
 
   let textProps = {
     fill: TEXT_FILL,
-    fontSize: 80,
+    fontSize: 69,
   };
 
   const wordsTxt = (
@@ -30,4 +32,17 @@ export default makeScene2D(function* (view) {
     />
   ) as Latex;
   wordsContainer.add(wordsTxt);
+
+  // 🎈 Words with variable names a_0, a_1, ...
+
+  const word1Variables = Array(5).fill(1).map(i => (
+    <Latex
+      tex={`a_{${i}}`}
+      {...textProps}
+      fill={TEXT_FILL_FADED}
+      fontSize={60}
+    />) as Latex,
+  );
+  console.log(word1Variables);
+  view.add([...word1Variables]);
 });
